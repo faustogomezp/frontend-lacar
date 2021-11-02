c38f4a7f37d95620c79f767c4f4752c05daa899f
fausto.gomez <fausto.gomez.pelaez@gmail.com>
2021-11-01 20:12:14 -0500

    Cambiamos el servidor para la llamada de la api

--- src/services/online/getDataVariable.js
+++ src/services/online/getDataVariable.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const getDataVariable = (logger) => {
     console.log(logger);
-    return axios.get(`http://localhost:3001/api/variables/online/${logger}`).then((response) => {
+    return axios.get(`http://neusapry.com:3001/api/variables/online/${logger}`).then((response) => {
         const { data } = response;
         return data;
     });

3550d93f0ef9d28694fef5e0ecf134686db2bb81
fausto.gomez <fausto.gomez.pelaez@gmail.com>
2021-11-05 15:48:34 -0500

    visualizamos el nivel del embalse

--- src/components/Reguladora.js
+++ src/components/Reguladora.js
@@ -12,13 +12,14 @@ export const Reguladora = () => {
         setVariable(variable);
     })
   }, [variable, logger])
+  console.log(variable['LIT_NEUSA']);
     return (
         <main className="Container">
             <h1>Embalse de Neusa</h1>
             <div className="Picture">
             <div className="Left">
                 <p className="Variable-title">Nivel</p>
-                <div className="Nivel-value"><p>{console.log(variable)}</p></div>
+                <div className="Nivel-value"><p>{variable['LIT_NEUSA']}</p></div>
                 <div className="Equipment"></div>
                 <div className="Line-Equipment"></div>
                 <div className="Water"></div>
